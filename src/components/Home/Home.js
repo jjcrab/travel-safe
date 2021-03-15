7ab34023350769e7e9b6f83228d2e380e9f31b9e
jjcrab <lily_lijj@hotmail.com>
2021-03-15 01:21:08 -0600

    modify layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,11 +5,11 @@ const Home = () => {
 	const url = process.env.PUBLIC_URL + `/images/`;
 	return (
 		<div className='home'>
-			<h1>
+			<h1 style={{ height: '10%' }}>
 				<span>Welcome to </span>
 				Travel Safe.
 			</h1>
-			<div style={{ width: '80%' }}>
+			<div style={{ width: '80%', height: '20%' }}>
 				<p>
 					Here you can find the up-to-date official travel advisories which are
 					issued by governments across the globe.
@@ -20,8 +20,13 @@ const Home = () => {
 				</p>
 				<p id='safetravel'>Safe travel!</p>
 			</div>
-
-			<img src={url + 'travelglobally.jpg'} alt='travel-globally' />
+			<div style={{ height: '60%' }}>
+				<img
+					src={url + 'travelglobally.jpg'}
+					alt='travel-globally'
+					id='homepageimg'
+				/>
+			</div>
 		</div>
 	);
 };
